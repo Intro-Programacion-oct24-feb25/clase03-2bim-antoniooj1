cb94c47a34f6cb6bc026af5a50dcf79c1664121f
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-12-18 11:47:22 -0500

    cambios for

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -28,12 +28,14 @@ public class Ejemplo04 {
         };
         
         int suma;
+        int sumaT =0;
         
         for (int fila = 0; fila < arreglo1.length; fila++) { // 0<2 // 1<2
             suma = 0;
             for (int col = 0; col < arreglo1[fila].length; col++) { // arreglo1[0].length:3 
                                                                     // 0<3 // 1<3 // 2<3 // 3<3
                 suma = suma + arreglo1[fila][col];
+                sumaT = sumaT + arreglo1[fila][col];
                      // 0 + arreglo1[0][0] // 0 + 10 // 10
                      // 10 + arreglo1[0][1] // 10 + 20 // 30
                      // 30 + arreglo1[0][2] // 30 + 40 // 70
@@ -41,7 +43,7 @@ public class Ejemplo04 {
             System.out.printf("Suma de valores del arreglo: %d\n", suma);
         }
         
-        // System.out.printf("Suma de valores del arreglo: %d\n", suma);
+         System.out.printf("Suma de valores del arreglo: %d\n", sumaT);
     }
     
 }
